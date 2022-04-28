fbe1525b0ff2b3a59acb6b3aa12e50d35adf0b7a
sony <sony@sony-PC>
2022-04-28 15:31:45 +0530

    fourth line

--- src/gitpractice/GitClass.java
+++ src/gitpractice/GitClass.java
@@ -5,6 +5,7 @@ public class GitClass {
 	public static void main(String[] args) {
 		System.out.println("first line");
 		System.out.println("Second Line");
+		System.out.println("fourth line");
 	}
 
 }
