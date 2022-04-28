f2dfe1c2db29cb7b5f9da45cd2c7c50c259c0998
admin <admin@DESKTOP-5NT9R5R>
2022-04-28 16:04:22 +0530

    00

--- src/gitpractice/GitClass.java
+++ src/gitpractice/GitClass.java
@@ -3,17 +3,7 @@ package gitpractice;
 public class GitClass {
 	
 	public static void main(String[] args) {
-		System.out.println("first line");
-		System.out.println("Second Line");
-		
-		System.out.println("Third line");
-		
-		System.out.println("sixth line");
-		
-		System.out.println("hiiii");
-		System.out.println("12");
-		
-		
+		System.out.println("00");
 	}
 
 }
