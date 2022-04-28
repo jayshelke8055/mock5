ca1dd07e62cdadb07d446828f43a339d3626b7ab
sony <sony@sony-PC>
2022-04-28 16:11:57 +0530

    sdgfd

--- src/gitpractice/GitClass.java
+++ src/gitpractice/GitClass.java
@@ -3,18 +3,9 @@ package gitpractice;
 public class GitClass {
 	
 	public static void main(String[] args) {
-		System.out.println("first line");
-		System.out.println("Second Line");
-
-		System.out.println("fourth line");
-		System.out.println("sixth line");
-
-		
-		System.out.println("Third line");
-		System.out.println("Fifth line");
-		System.out.println("Sixth Line");
 		
 		
+		System.out.println("@@@@@@");
 
 	}
 
