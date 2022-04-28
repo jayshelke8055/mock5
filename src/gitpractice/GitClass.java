8f8f500b0fe56b398effa67201d8fa192851557f
admin <admin@DESKTOP-5NT9R5R>
2022-04-28 15:26:43 +0530

    third program

--- src/gitpractice/GitClass.java
+++ src/gitpractice/GitClass.java
@@ -5,6 +5,10 @@ public class GitClass {
 	public static void main(String[] args) {
 		System.out.println("first line");
 		System.out.println("Second Line");
+		
+		System.out.println("Third line");
+		
+		
 	}
 
 }
