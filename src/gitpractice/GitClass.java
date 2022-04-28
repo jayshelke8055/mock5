82741375cebd306c2ce1a8c2a9ec3de7a89824bd
admin <admin@admin-PC>
2022-04-28 22:25:28 +0530

    blank

--- src/gitpractice/GitClass.java
+++ src/gitpractice/GitClass.java
@@ -21,7 +21,7 @@ public class GitClass {
 		System.out.println("It was Sweet");
 		System.out.println("It was Bitter");
 		System.out.println("bye bye all");
-		
+		System.out.println("blank");
 		
 		
 
