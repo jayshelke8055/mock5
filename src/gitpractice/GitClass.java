0bd0b21fb5c25ce578cf2f0d777c59637991a25f
admin <admin@admin-PC>
2022-04-28 19:29:08 +0530

    goodeve

--- src/gitpractice/GitClass.java
+++ src/gitpractice/GitClass.java
@@ -14,18 +14,8 @@ public class GitClass {
 		
 		System.out.println("yesss");
 		
-		System.out.println("123");
-
-		System.out.println("Third line");
-		System.out.println("Fifth line");
-		System.out.println("Sixth Line");
-
-		System.out.println("Hello");
+		System.out.println("Good Evening");
 		
-		System.out.println("Hi Gourav sir");
-		
-		
-		System.out.println("hello");
 		
 
 	}
