5fce64357cd5e15f57bdab17554a654c073b9c7f
admin <admin@DESKTOP-5NT9R5R>
2022-04-28 16:28:44 +0530

    jk

--- src/gitpractice/GitClass.java
+++ src/gitpractice/GitClass.java
@@ -6,20 +6,9 @@ public class GitClass {
 
 		System.out.println("00");
 
-		System.out.println("first line");
-		System.out.println("Second Line");
+		System.out.println("1234");
 		
-
-		System.out.println("seventh line");
-		
-		System.out.println("yesss");
-		
-		System.out.println("123");
-
-		System.out.println("Third line");
-		System.out.println("Fifth line");
-		System.out.println("Sixth Line");
-		
+		System.out.println("123456789");
 		
 
 	}
