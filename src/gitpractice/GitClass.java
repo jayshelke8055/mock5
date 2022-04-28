67de37c1e79bd1a10fed2073456668431236bd5a
admin <admin@DESKTOP-5NT9R5R>
2022-04-28 15:51:20 +0530

    yes

--- src/gitpractice/GitClass.java
+++ src/gitpractice/GitClass.java
@@ -5,6 +5,9 @@ public class GitClass {
 	public static void main(String[] args) {
 		System.out.println("first line");
 		System.out.println("Second Line");
+		
+		System.out.println("seventh line");
 	}
+	
 
 }
